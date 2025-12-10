19b5688ef1f7b4beb719bc9f9c61dd08b2ad5749
Yee Mey Seah <ymseah@amarel2.amarel.rutgers.edu>
2025-12-10 18:44:48 -0500

    Batch job scripts to run on cluster

---
+++ RosettaDDGPrediction/job-script_rosetta_ddg_run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH --partition=main          # Partition (job queue)
+#SBATCH --requeue                 # Return job to the queue if preempted
+#SBATCH --job-name=1al0f       # Assign a short name to your job
+#SBATCH --nodes=1                 # Number of nodes you require
+#SBATCH --ntasks=1                # Total # of tasks across all nodes
+#SBATCH --cpus-per-task=1         # Cores per task (>1 if multithread tasks)
+#SBATCH --mem=6000                # Real memory (RAM) required (MB)
+#SBATCH --time=3-00:00:00           # Total run time limit (HH:MM:SS)
+#SBATCH --output=slurm.%N.%j.out  # STDOUT output file
+#SBATCH --error=slurm.%N.%j.err   # STDERR output file (optional)
+
+cd /scratch/$USER/RosettaDDGPrediction/RosettaDDGPrediction/ 
+
+srun rosetta_ddg_run -p data/1AL0_chainF.pdb -cr config_run/cartddg2020_ref2015.yaml -cs config_settings/nompi.yaml -r /scratch/$USER/rosetta.binary.ubuntu.release-408/ -d output/ -l data/1AL0_F_mutations.txt
+
