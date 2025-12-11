34e5424782baa9accb21283177cced81ab01d8ba
Yee Mey Seah <ymseah@amarel2.amarel.rutgers.edu>
2025-12-11 11:35:18 -0500

    Update cpus per task

--- RosettaDDGPrediction/job-script_rosetta_ddg_run.sh
+++ RosettaDDGPrediction/job-script_rosetta_ddg_run.sh
@@ -5,7 +5,7 @@
 #SBATCH --job-name=1al0f       # Assign a short name to your job
 #SBATCH --nodes=1                 # Number of nodes you require
 #SBATCH --ntasks=1                # Total # of tasks across all nodes
-#SBATCH --cpus-per-task=1         # Cores per task (>1 if multithread tasks)
+#SBATCH --cpus-per-task=6         # Cores per task (>1 if multithread tasks)
 #SBATCH --mem=6000                # Real memory (RAM) required (MB)
 #SBATCH --time=3-00:00:00           # Total run time limit (HH:MM:SS)
 #SBATCH --output=slurm.%N.%j.out  # STDOUT output file
@@ -13,5 +13,5 @@
 
 cd /scratch/$USER/RosettaDDGPrediction/RosettaDDGPrediction/ 
 
-srun rosetta_ddg_run -p data/1AL0_chainF.pdb -cr config_run/cartddg2020_ref2015.yaml -cs config_settings/nompi.yaml -r /scratch/$USER/rosetta.binary.ubuntu.release-408/ -d output/ -l data/1AL0_F_mutations.txt
+srun rosetta_ddg_run -p data/1AL0_chainF.pdb -cr config_run/cartddg2020_ref2015.yaml -cs config_settings/nompi.yaml -r /scratch/$USER/rosetta.binary.ubuntu.release-408/ -d output/ -l data/1AL0_F_mutations.txt -n 6
 
